d1a4b1821cc0f768f1050854e7564eaff03205ed
bib2cky <151136536+bib2cky@users.noreply.github.com>
2024-07-05 19:57:05 +0300

    React Essential - Components, JSX, Props, State & More

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,9 @@
-export default function TabButton({ children }) {
-    function handleClick() {
-        console.log('Hello World');
-    }
-
-    return (
-        <li>
-            <button onClick={handleClick}>{children}</button>
-        </li>
-    );
+export default function TabButton({ children, onSelect, isSelected }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
 }
